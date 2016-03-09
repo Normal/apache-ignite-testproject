4d539a7acd5f8666226af9ab16ce54ff5e8ac618
George Yarish <georgii_iarysh@epam.com>
2016-03-09 19:56:50 +0300

    added caching sample

--- src/main/java/ApplicationConfiguration.java
+++ src/main/java/ApplicationConfiguration.java
@@ -1,14 +1,28 @@
+import org.apache.ignite.cache.CacheMode;
+import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = "service")
 public class ApplicationConfiguration {
 
     @Bean
-    public IgniteConfiguration igniteConfiguration() {
-        IgniteConfiguration configuration = new IgniteConfiguration();
+    public IgniteConfiguration igniteConfiguration(CacheConfiguration cache) {
+        IgniteConfiguration conf = new IgniteConfiguration();
+        conf.setCacheConfiguration(cache);
 
-        return configuration;
+        return conf;
+    }
+
+    @Bean
+    public CacheConfiguration cacheConfiguration() {
+        CacheConfiguration cache = new CacheConfiguration();
+
+        cache.setCacheMode(CacheMode.REPLICATED);
+
+        return cache;
     }
 }
---
+++ src/main/java/service/Person.java
@@ -0,0 +1,14 @@
+package service;
+
+public class Person {
+
+    public String name;
+    public Integer salary;
+    public Integer age;
+
+    public Person(String name, Integer salary, Integer age) {
+        this.name = name;
+        this.salary = salary;
+        this.age = age;
+    }
+}
