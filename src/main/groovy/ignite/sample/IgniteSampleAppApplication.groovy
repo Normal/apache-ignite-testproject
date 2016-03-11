7bb1422351c3f243eb7be2d68c7c7fa58888e93e
George Yarish <georgii_iarysh@epam.com>
2016-03-11 19:41:54 +0300

    init spring boot web application

---
+++ src/main/groovy/ignite/sample/IgniteSampleAppApplication.groovy
@@ -0,0 +1,12 @@
+package ignite.sample
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class IgniteSampleAppApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run IgniteSampleAppApplication, args
+	}
+}
